3c072963a9038096fc273396a8ef9ba7d5d8d395
aprxty3 <ajiprxty3@gmail.com>
2023-07-13 18:36:12 +0700

    create some widget

--- lib/application/screen/advice/advice_screen.dart
+++ lib/application/screen/advice/advice_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc_architecture/application/core/service/theme_service.dart';
+import 'package:flutter_bloc_architecture/application/screen/advice/widgets/advice_field.dart';
+import 'package:flutter_bloc_architecture/application/screen/advice/widgets/custom_button.dart';
+import 'package:flutter_bloc_architecture/application/screen/advice/widgets/erro_message.dart';
 import 'package:provider/provider.dart';
 
 class AdviceScreen extends StatelessWidget {
@@ -7,11 +10,12 @@ class AdviceScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Advice Screen',
-          style: Theme.of(context).textTheme.headline1,
+          style: themeData.textTheme.headline1,
         ),
         centerTitle: true,
         actions: [
@@ -22,7 +26,33 @@ class AdviceScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Scaffold(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 50),
+        child: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: ErrorMessage(message: 'example error message'),
+                // AdviceField(advice: 'example advice'),
+                //     CircularProgressIndicator(
+                //   color: themeData.colorScheme.secondary,
+                // )
+                // Text(
+                //   'Your Advice is Waiting',
+                //   style: themeData.textTheme.headline1,
+                //   textAlign: TextAlign.center,
+                // ),
+              ),
+            ),
+            const SizedBox(
+              height: 200,
+              child: Center(
+                child: CustomButton(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
