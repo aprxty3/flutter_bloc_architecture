1f8b0bffa37ac5ad72dcca2846f0f96f00061e53
aprxty3 <ajiprxty3@gmail.com>
2023-07-13 21:48:34 +0700

    implement the function

--- lib/application/screen/advice/advice_screen.dart
+++ lib/application/screen/advice/advice_screen.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_architecture/application/core/service/theme_service.dart';
+import 'package:flutter_bloc_architecture/application/screen/advice/bloc/advice_bloc.dart';
 import 'package:flutter_bloc_architecture/application/screen/advice/widgets/advice_field.dart';
 import 'package:flutter_bloc_architecture/application/screen/advice/widgets/custom_button.dart';
 import 'package:flutter_bloc_architecture/application/screen/advice/widgets/erro_message.dart';
 import 'package:provider/provider.dart';
+
+class AdviceScreenWrapperProvider extends StatelessWidget {
+  const AdviceScreenWrapperProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => AdviceBloc(),
+      child: AdviceScreen(),
+    );
+  }
+}
 
 class AdviceScreen extends StatelessWidget {
   const AdviceScreen({super.key});
@@ -32,16 +46,26 @@ class AdviceScreen extends StatelessWidget {
           children: [
             Expanded(
               child: Center(
-                child: ErrorMessage(message: 'example error message'),
-                // AdviceField(advice: 'example advice'),
-                //     CircularProgressIndicator(
-                //   color: themeData.colorScheme.secondary,
-                // )
-                // Text(
-                //   'Your Advice is Waiting',
-                //   style: themeData.textTheme.headline1,
-                //   textAlign: TextAlign.center,
-                // ),
+                child: BlocBuilder<AdviceBloc, AdviceState>(
+                  builder: (context, state) {
+                    if (state is AdviceInitial) {
+                      return Text(
+                        'Your Advice is Waiting',
+                        style: themeData.textTheme.headline1,
+                        textAlign: TextAlign.center,
+                      );
+                    } else if (state is AdviceStateLoading) {
+                      return CircularProgressIndicator(
+                        color: themeData.colorScheme.secondary,
+                      );
+                    } else if (state is AdviceStateLoaded) {
+                      return AdviceField(advice: state.advice);
+                    } else if (state is AdviceStateError) {
+                      return ErrorMessage(message: state.message);
+                    }
+                    return const SizedBox();
+                  },
+                ),
               ),
             ),
             const SizedBox(
