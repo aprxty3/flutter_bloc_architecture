89796391666aea143051381a0ed17aa3d3da3a9f
aprxty3 <ajiprxty3@gmail.com>
2023-07-14 19:00:27 +0700

    use equatable

--- lib/application/screen/advice/bloc/advice_event.dart
+++ lib/application/screen/advice/bloc/advice_event.dart
@@ -1,5 +1,8 @@
 part of 'advice_bloc.dart';
 
-abstract class AdviceEvent {}
+abstract class AdviceEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 class AdviceRequestedEvent extends AdviceEvent {}
